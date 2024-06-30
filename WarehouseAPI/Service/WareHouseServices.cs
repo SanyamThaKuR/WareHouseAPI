c8bfc391ba76114052685a1f18eb5404ac3e92c0
Sanyam Thakur <sanyamthakur003@gmail.com>
2024-06-30 23:58:10 +0530

    3 commit

--- WarehouseAPI/Service/WareHouseServices.cs
+++ WarehouseAPI/Service/WareHouseServices.cs
@@ -1,11 +1,50 @@
 ﻿using System;
+using WarehouseAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Created originally by ==================> SANYAM
+
 namespace WarehouseAPI.Service
 {
-	public class WareHouseServices
-	{
-		public WareHouseServices()
-		{
-		}
-	}
+    public class WareHouseServices : IWareHouseServices
+    {
+
+        private readonly WareHouseContext _context;
+
+        // default constructor to ini. the context
+        public WareHouseServices(WareHouseContext context) { _context = context; }
+
+        // Add warehouse to dbset
+        public void AddWarehouse(WareHouse warehouse)
+        {
+            _context.Warehouses.Add(warehouse);   // add to dbset
+            _context.SaveChanges();
+        }
+
+        public void DeleteWarehouse(int id)
+        {
+            var delete = _context.Warehouses.Where(b => b.ID == id).FirstOrDefault();
+            //base condition ======>
+            if(delete != null) { _context.Warehouses.Remove(delete); }
+                
+        }
+
+        public IEnumerable<WareHouse> GetAllWareHouses()
+        {
+            return _context.Warehouses.ToList();
+        }
+
+        public WareHouse GetWarehouseByNumber(int warehouseNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWarehouse(WareHouse warehouse)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
