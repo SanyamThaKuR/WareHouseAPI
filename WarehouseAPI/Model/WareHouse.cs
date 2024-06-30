8901331769159ad8cbcdbde7e225c41b7640b7a1
Sanyam Thakur <sanyamthakur003@gmail.com>
2024-06-30 23:12:15 +0530

    Your commit message here

--- WarehouseAPI/Model/WareHouse.cs
+++ WarehouseAPI/Model/WareHouse.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WarehouseAPI.Model
 {
 	public class WareHouse
 	{
-		public WareHouse()
-		{
-		}
-	}
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+       public string name { get; set; }
+        [Required(ErrorMessage = "Number is required.")]
+        public int WareHouseNumber { get; set; }
+        
+    }
 }
 
---
+++ WarehouseAPI/Service/IWareHouseServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WarehouseAPI.Model;
+
+
+// Created originally by ==================> SANYAM
+namespace WarehouseAPI.Service
+{
+	public interface IWareHouseServices
+	{
+        //Retrieves all warehouses.
+        public IEnumerable<WareHouse> GetAllWareHouses();
+
+        //Retrieves a specific warehouse by its warehouse number.
+        public WareHouse GetWarehouseByNumber(int warehouseNumber);
+
+        //Adds a new warehouse to the system.
+        void AddWarehouse(WareHouse warehouse);
+
+        //Update a new warehouse to the system.
+        void UpdateWarehouse(WareHouse warehouse);
+
+        //Delete data.
+        void DeleteWarehouse(int warehouseNumber);// using ID column
+
+
+
+    }
+}
+
