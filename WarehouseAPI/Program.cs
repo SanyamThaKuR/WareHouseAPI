5a5527a157def0e718bb52ff846f33e1245c3493
Sanyam Thakur <sanyamthakur003@gmail.com>
2024-07-01 01:29:44 +0530

    5 commit

--- WarehouseAPI/Program.cs
+++ WarehouseAPI/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace WarehouseAPI;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using WarehouseAPI.Service;
+
+namespace WarehouseAPI;
 
 public class Program
 {
@@ -6,25 +13,31 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-
+        //===============================================
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        //===============================================
+        //builder.Services.AddDbContext<WareHouseContext>(option =>
+        // option.UseSqlServer(//TODO)
+        //================================================
+        builder.Services.AddScoped<IWareHouseServices, WareHouseServices>(); // Registers IWarehouseService with  dependency injection.
+        var app = builder.Build(); // Builds the app.
 
-        var app = builder.Build();
-
+        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage(); // Provides detailed exception pages in development.
+            app.UseSwagger(); // Enables Swagger middleware to serve generated Swagger as JSON endpoint.
+            app.UseSwaggerUI(); // Enables Swagger UI.
         }
 
-        app.UseAuthorization();
+        app.UseRouting(); // Enables routing.
+        app.UseAuthorization(); // Enables authorization.
+        app.MapControllers(); // Maps controller endpoints.
+        app.Run(); // Runs the app.
 
 
-        app.MapControllers();
-
-        app.Run();
     }
 }
 
